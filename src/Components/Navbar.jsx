a29b8b0e0570a8a10f8ea8214537a9fbf1e7ea60
Kenz <oakleysdmr@gmail.com>
2022-01-29 15:51:49 -0800

    header

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../images/logo.png";
+import { Link } from "react-scroll";
 
 function Navbar() {
-  return <div></div>;
+  const [nav, setnav] = useState(false);
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setnav(true);
+    } else {
+      setnav(false);
+    }
+  };
+  window.addEventListener("scroll", changeBackground);
+  return (
+    <nav className={nav ? "nav active" : "nav"}>
+      <Link to="#" className="logo">
+        <img src={logo} alt="React fitness logo" />
+      </Link>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" for="menu-btn">
+        <span className="nav-icon"></span>
+      </label>
+      <ul className="menu">
+        <li>
+          <Link to="#" />
+          Header
+        </li>
+        <li>
+          <Link to="#" />
+          Features
+        </li>
+        <li>
+          <Link to="#" />
+          Offer
+        </li>
+        <li>
+          <Link to="#" />
+          About
+        </li>
+        <li>
+          <Link to="#" />
+          Contact
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
 export default Navbar;
